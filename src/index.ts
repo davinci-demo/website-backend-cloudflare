11ddbd8fdc33bdab138f6edcffcd146241a74747
defranciscon <defrancisco.nicolas@gmail.com>
2024-01-18 19:09:39 -0500

    Added models, API routes, Auth middleware, Role definitions.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,68 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 const express = require('express');
 const dotenv = require('dotenv');
 
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression'
+import cors from 'cors';
+import helmet from 'helmet';
+
+import authRouter from './routes/auth.routes';
+import courseRouter from './routes/course.routes';
+import enrollmentRouter from './routes/enrollment.routes';
+import userRouter from './routes/users.routes';
 import { connectDB } from './config/database';
-import router from './routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+
+app.use('/api', (req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+})
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('DaVinci API Backend Works');
+  res.send('Davinci API Backend Works');
 });
 
 // Connect to database
 connectDB();
 
-// Routing for API routes
-app.use('/api', router);
+// app routers
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/courses', courseRouter);
+app.use('/api/enrollment', enrollmentRouter);
 
-// Routing for Next.js pages
-// app.get('*', (req: Request, res: Response) => {
-//   return handle(req, res);
-// });
+/* 
+ * app.get('*', (req: Request, res: Response) => {
+ *   return handle(req, res);
+ * })
+ */
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({"error": err.name + ": " + err.message})
+  } else if (err) {
+    res.status(400).json({"error": err.name + ": " + err.message})
+    console.log(err)
+  }
+})
 
 app.listen(port, () => {
   console.log(`🚀 Server is running at http://localhost:${port}`);
 });
+
+export default app;
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,21 +2,53 @@ import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
     name: {
-        type: String,
-        required: true
+        first: { 
+            type: String, 
+            required: true,
+            trim: true
+        },
+        last: {
+            type: String,
+            required: true,
+            trim: true
+        }
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true,
+        lowercase: true,
     },
-    password: {
+    hashed_password: {
         type: String,
-        required: true
-    }
+        required: true, // required is true temporarily until external identity providers implemented
+        trim: true
+    },
+    updated: Date,
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    role: {
+        _id: {
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+    },
+});
+
+userSchema.virtual('role_name').get(function() {
+    return `${this.role.name}`;
 });
 
 userSchema.index({ email: 1 });
 
 const User = model('User', userSchema);
+
 export default User;
