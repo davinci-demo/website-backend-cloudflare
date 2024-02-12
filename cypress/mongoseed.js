4076f58d5b5d37d01365489009265b1cd1a6f077
興怡 <>
2024-02-12 15:31:28 -0800

    mongosh, mongo seed data pt13

--- cypress/mongoseed.js
+++ cypress/mongoseed.js
@@ -1,19 +1,15 @@
-import {User} from "./compileduser.js";
-
-const mongoose = require("mongoose");
 
 const MONGODB_URI = process.env.MONGODB_URI;
+db = connect( MONGODB_URI );
 
 // generate one record
-const genOne = () => {
-    const accumulator = [];
-    const name = {first: "example", last: "test"};
-    email = "example@davincirenaissance.org";
-    hash = "ABCDEF123456789";
-    stamp = Date.now();
-    role = {_id: "507f1f77bcf86cd799439011", name: "instructor"};
-
-    accumulator.push({
+const accumulator = [];
+const name = {first: "example", last: "test"};
+const email = "example@davincirenaissance.org";
+const hash = "ABCDEF123456789";
+const stamp = Date.now();
+const role = {name: "instructor"};
+accumulator.push({
       name,
       email,
       hash,
@@ -21,23 +17,6 @@ const genOne = () => {
       stamp,
       role,
     });
-    return accumulator;
-}
 
-const connectDB = async () => {
-    try {
-      await mongoose.connect(MONGODB_URI);
-      console.log("connected to db");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-const sample = genOne();
-connectDB();
-User.insertMany(sample)
-    .then(docs => console.log("insert done"))
-    .catch(err => {
-      console.error(err);
-    });
+db.users.insertMany( accumulator );
 
