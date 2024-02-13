bcab6683c91576d6f2c61606fb6f75b6a1c29ee6
興怡 <>
2024-02-12 18:40:11 -0800

    try -u and -p with mongosh, mongo seed data pt21

--- cypress/mongoseed.js
+++ cypress/mongoseed.js
@@ -1,6 +1,6 @@
 
-const MONGODB_URI = process.env.MONGODB_URI;
-db = connect( MONGODB_URI );
+////const MONGODB_URI = process.env.MONGODB_URI;
+db = connect( 'mongodb://localhost:27017/APP-DATABASE' );
 
 // generate one record
 const accumulator = [];
